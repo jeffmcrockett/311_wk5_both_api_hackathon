49399dea9709ad8715b1c60fb56ccb708c79ceb7
Jeff Crockett <jeffmcrockett@gmail.com>
2021-04-22 09:32:34 -0500

    adding Yousif's GH db

--- sql/connection.js
+++ sql/connection.js
@@ -6,10 +6,10 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'hackathon3'
+        host: 'den1.mysql3.gear.host',
+        user: 'ysdb1',
+        password: 'adminadmin1!',
+        database: 'ysdb1'
       })
 
       return this.pool
