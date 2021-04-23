786735da772dc083b061a73f4c26424c655f3644
Cesar Cisneros <cesarcisneros9@gmail.com>
2021-04-22 19:59:01 -0500

    added connection to sql

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controller/controller');
-const router = express.Router();
-
-router.get('/', controller.get);
-    // Since we only need a get, is this the only route we need?
-
-module.exports = router;
