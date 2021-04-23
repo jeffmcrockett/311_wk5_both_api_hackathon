4518c201a6c6b663f5d90ee380b212184f5a854f
Cesar Cisneros <cesarcisneros9@gmail.com>
2021-04-22 20:46:06 -0500

    first logic

--- sql/connection.js
+++ sql/connection.js
@@ -15,26 +15,4 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId)
 });
 
-
-// class Connection {
-//   constructor() {
-//     if (!this.pool) {
-//       console.log('creating connection...')
-//       this.pool = mysql.createPool({
-//         connectionLimit: 100,
-//         host: 'den1.mysql3.gear.host',
-//         user: 'hackathon31',
-//         password: 'Tp4iMH!!vaYG',
-//         database: 'hackathon31'
-//       })
-
-//        return this.pool
-//     }
-
-//     return this.pool
-//   }
-// }
-
-// const instance = new Connection()
-
-module.exports = instance;
+module.exports = connection;
