09ed9d542a1125c718c0af3750b9c0b0c9efbeb7
James Messina <jmessina17@gmail.com>
2021-04-22 20:16:26 -0500

    connection file added

--- sql/connection.js
+++ sql/connection.js
@@ -16,25 +16,4 @@ connection.connect(function(err) {
 });
 
 
-// class Connection {
-//   constructor() {
-//     if (!this.pool) {
-//       console.log('creating connection...')
-//       this.pool = mysql.createPool({
-//         connectionLimit: 100,
-//         host: 'den1.mysql3.gear.host',
-//         user: 'hackathon31',
-//         password: 'Tp4iMH!!vaYG',
-//         database: 'hackathon31'
-//       })
-
-//        return this.pool
-//     }
-
-//     return this.pool
-//   }
-// }
-
-// const instance = new Connection()
-
-module.exports = instance;
+module.exports = connection;
